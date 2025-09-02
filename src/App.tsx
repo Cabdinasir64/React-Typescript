f40d92b284b7aa53102cb45fff31d06a40022da5
Cabdinasir64 <cabdinasira64@gmail.com>
2025-09-02 16:32:31 +0300

    feat: add Example1 component to App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
-
+import Example1 from'./State & Events/example1';
 
 const App: React.FC = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center space-y-4">
+      <Example1 />
     </div>
   );
 };
