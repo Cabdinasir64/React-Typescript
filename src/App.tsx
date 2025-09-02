c0f5bf93ee6eef4d2d86b132118fb85739cc2983
Cabdinasir64 <cabdinasira64@gmail.com>
2025-09-02 13:57:37 +0300

    feat: add initial App component with Example1 integration

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import Example1 from './Basic/example1';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <Example1 />
+    </div>
+  );
+};
+
+export default App;
