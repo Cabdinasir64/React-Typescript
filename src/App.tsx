585884ea59f6ee2476b3bcf511dac0b0d5ca8098
Cabdinasir64 <cabdinasira64@gmail.com>
2025-09-02 15:21:06 +0300

    feat: update App component layout and add Example5 button examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import Example5 from './Basic/example5'
 
 const App: React.FC = () => {
   return (
-    <div>
-     
+    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center space-y-4">
+      <Example5 label="Default Button" />              
+      <Example5 label="Small Button" size="small" />  
+      <Example5 label="Large Button" size="large" /> 
+      <Example5 label="Disabled Button" disabled />   
     </div>
   );
 };
