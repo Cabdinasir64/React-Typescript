8ae8220e9e60eaa89b8f9cfe0dcc1d13e5ebb82f
Cabdinasir64 <cabdinasira64@gmail.com>
2025-09-02 15:08:21 +0300

    feat: remove Example4 component and users data from App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
 import React from 'react';
-import Example4 from './Basic/example4';
-
-const usersData = [
-  { id: 1, name: "Abdinasir", age: 21, isStudent: true },
-  { id: 2, name: "Amina", age: 25, isStudent: false },
-  { id: 3, name: "Mohamed", age: 19, isStudent: true },
-];
 
 const App: React.FC = () => {
   return (
     <div>
-      <Example4 users={usersData} />
+     
     </div>
   );
 };
