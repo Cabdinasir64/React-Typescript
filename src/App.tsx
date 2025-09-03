0b28212c03a82849f818be80b1734ddcdbb41ec3
Cabdinasir64 <cabdinasira64@gmail.com>
2025-09-03 11:42:02 +0300

    i make update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import { UserProvider } from './Context, Reducer & Custom Hooks/example2'
 import { Example3 } from './Context, Reducer & Custom Hooks/example3'
 const App: React.FC = () => {
   return (
-    <UserProvider>
       <Example3 />
-    </UserProvider>
   );
 };
 
