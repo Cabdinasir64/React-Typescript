0605e3385cb01975355f7634f3e3be798d1c7db2
Cabdinasir64 <cabdinasira64@gmail.com>
2025-09-03 11:19:23 +0300

    i make update

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
-import Example4 from './Lists & Generics/example4'
+import Example1 from './Context, Reducer & Custom Hooks/example1'
 const App: React.FC = () => {
   return (
-    <Example4 />
+    <Example1 />
   );
 };
 
