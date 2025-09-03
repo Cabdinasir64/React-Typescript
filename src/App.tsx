9de7c2a3bc1e01b70a5316cb7f5fdad3b428f527
Cabdinasir64 <cabdinasira64@gmail.com>
2025-09-03 09:01:54 +0300

    i make update

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,8 @@
 import React from 'react';
-import Example2 from'./Component Composition & Props Advanced/example2';
-
+import Example3 from './Component Composition & Props Advanced/example3'
 const App: React.FC = () => {
   return (
-    <div className="min-h-screen bg-gray-100 flex flex-col items-center justify-center space-y-4">
-      <Example2 />
-    </div>
+    <Example3 />
   );
 };
 
